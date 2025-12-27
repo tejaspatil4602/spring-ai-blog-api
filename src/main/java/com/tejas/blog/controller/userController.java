fc51407716acd44b39bef382dbd19fe2e19bc8bd
tejaspatil4602 <tejaspatil4602@gmail.com>
2025-12-27 14:21:54 +0530

    Tested all apis and create a new api response class to handle a specific response for user

--- src/main/java/com/tejas/blog/controller/userController.java
+++ src/main/java/com/tejas/blog/controller/userController.java
@@ -1,16 +1,24 @@
 package com.tejas.blog.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tejas.blog.payload.ApisResponse;
 import com.tejas.blog.payload.UserDto;
+import com.tejas.blog.payload.apisResponse;
 import com.tejas.blog.services.UserService;
 
 @RestController
@@ -27,4 +35,36 @@ public class userController {
 		 UserDto createUserDto = this.userService.createUser(userDto);
 		 return new ResponseEntity<>(createUserDto,HttpStatus.CREATED);
 	 }
-} 
+	 //Update User
+	 @PutMapping("/{userId}")
+	 public ResponseEntity<UserDto> updateUser (@RequestBody UserDto userDto ,@PathVariable Integer userId){
+		 UserDto updatedUser =this.userService.updateUser(userDto, userId );
+		 return ResponseEntity.ok(updatedUser);
+	 }
+	 
+	 //delete User
+	 @DeleteMapping("/{userId}")
+	 public ResponseEntity<ApisResponse> deleteUser (@PathVariable Integer userId ){
+		 this.userService.deleteUser(userId);
+		 return new ResponseEntity< ApisResponse>(new ApisResponse("User delete Successfully ", true), HttpStatus.OK);
+	 }
+	 //getAllUser
+	 @GetMapping("/")
+	 public ResponseEntity<List<UserDto>> getAllUser(){
+		 return ResponseEntity.ok(this.userService.getAllUser());
+	 }
+	 
+	 //getUserById
+	 @GetMapping("/{userId}") 
+	 public ResponseEntity<UserDto> getBySingleId(@PathVariable Integer userId){
+		 return ResponseEntity.ok(this.userService.getUserById(userId));
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+}
---
+++ src/main/java/com/tejas/blog/payload/ApisResponse.java
@@ -0,0 +1,15 @@
+package com.tejas.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApisResponse {
+	private String massage;
+	private boolean success;
+}
